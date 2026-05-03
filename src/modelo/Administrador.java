3a29b1746ae5d190f4054cbc28a61ea1a34995dc
John_Lacoste <ja.acostar1@uniandes.edu.co>
2026-05-03 16:38:53 -0500

    ss

--- src/modelo/Administrador.java
+++ src/modelo/Administrador.java
@@ -1,5 +1,5 @@
 package modelo;
-
+ //#endregion  
 public class Administrador extends Usuario {
     public Administrador(String login, String password, String id) {
         super(login, password, id);
